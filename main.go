a8ceee77fca186772e4e0e5173bee391d8f9b399
HTJoker <hersh.thakur@gmail.com>
2024-11-26 16:38:15 +0000

    boilerplate for http server complete

--- main.go
+++ main.go
@@ -12,8 +12,6 @@ import (
 )
 
 func main() {
-	fmt.Println("hello world")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -30,6 +28,12 @@ func main() {
 		ExposedHeaders: []string{"Link"},
 		MaxAge:         300,
 	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
